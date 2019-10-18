1f63daec12a6411cd03357b003ce9a6a82e60216
tprotic <tprotic@ncsu.edu>
2019-10-18 01:47:02 -0400

    created conversation for invalid cases of level 0

--- index.js
+++ index.js
@@ -134,12 +134,59 @@ controller.hears('hello', 'direct_message', function (bot, message) {
 
 
 //pushing level value 0 in db
-var level=0;
+var level = 1;
 //Start Convo
 controller.hears('start', 'direct_message', function (bot, message){
-    bot.reply(message,'Welcome');
-    bot.reply(message,'Please say I am ready when you are ready');
-    level++;
+    if (level === 0) {
+        bot.reply(message,'Welcome!');
+        bot.reply(message,'Please say I am ready when you are ready');
+        level++;
+    } else {// bot replies an error message when the user is not in level 0
+        bot.createConversation(message, function(err, convo) {
+            // create a path for when a user says YES
+            convo.addMessage({
+                    text: 'Please say \'start\' to start a new session.',
+            },'yes_thread');
+        
+            // create a path for when a user says NO
+            convo.addMessage({
+                text: 'Alright',
+            },'no_thread');
+        
+            // create a path where neither option was matched
+            // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+            convo.addMessage({
+                text: 'Sorry I did not understand.',
+                action: 'default',
+            },'bad_response');
+        
+            // Create a yes/no question in the default thread...
+            convo.addQuestion('A session is already going on. Do you want to start a new session [y/n]?', [
+                {
+                    pattern: 'y',
+                    callback: function(response, convo) {
+                        convo.gotoThread('yes_thread');
+                        level = 0;
+                    },
+                    
+                },
+                {
+                    pattern: 'n',
+                    callback: function(response, convo) {
+                        convo.gotoThread('no_thread');
+                    },
+                },
+                {
+                    default: true,
+                    callback: function(response, convo) {
+                        convo.gotoThread('bad_response');
+                    },
+                }
+            ],{},'default');
+        
+            convo.activate();
+        });
+    }
 });
 
 controller.hears('I am ready','direct_message', function(bot, message){
