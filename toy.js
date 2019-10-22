13ca907164a4ec047f1e84f632cdd3d44f25fc22
mhaque3 <mhaque3@ncsu.edu>
2019-10-22 15:27:21 -0400

    merfe conflict

--- toy.js
+++ toy.js
@@ -17,3 +17,21 @@ const file = fs.createWriteStream("file.yml");
 const request = http.get("https://transfer.sh/DotXl/bot.yml", function(response) {
   response.pipe(file);
 });
+
+
+function getDblpData(userName){
+  const url = dblpUrl + '/search/publ/api?q==author:' + response + ":&format=json";
+	const options = {
+		method: 'GET',
+		headers: {
+			"content-type": "application/json"
+		},
+		json: true
+	};
+
+  let profile_details = ( http_request(url, options)).body;
+  return profile_details;
+}
+
+exports.getDblpData = getDblpData
+
