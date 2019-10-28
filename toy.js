fed34f02d1f7444986c179135c44e7e7069da0ed
Md Rayhanur Rahman <mrahman@ncsu.edu>
2019-10-28 10:30:02 -0400

    mongodb connection done with promise

--- toy.js
+++ toy.js
@@ -16,21 +16,68 @@ function upload() {
 }
 
 function upload() {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         request.post('https://0x0.st', function (err, resp, body) {
-        if (err) {
-            console.log('Error!');
-            reject(err);
-        } else {
-            resolve(body);
-        }
-    }).form().append('file', fs.createReadStream('util.js'));
-    } );
+            if (err) {
+                console.log('Error!');
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        }).form().append('file', fs.createReadStream('util.js'));
+    });
 }
 
-async function test(){
+async function test() {
     var x = await upload();
     console.log(x);
 }
 
-test();
+require('dotenv').config();
+
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+
+// Connect to the db
+function connectToMongo() {
+    return MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true }).then(client => {
+        var dbo = client.db('heroku_wvfjdtzx');
+        console.log('db connected');
+        return dbo;
+
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function createCollection(dbo){
+    return dbo.createCollection('dogs').then(result => {
+        console.log('collection created!');
+    }).catch(err => {
+        console.log('error');
+    });
+}
+
+function insert(dbo){
+    var collection = dbo.collection('dogs');
+    return collection.insertOne({name: 'Darth', id: 3}).then(res => console.log('inserted')).catch(err => console.log(err));
+}
+
+function find(dbo){
+    var collection = dbo.collection('dogs');
+    return collection.findOne({name: 'Alice'}).then(res => {
+        return res;
+    }).catch(err => console.log(err));
+}
+
+async function dosth() {
+    var dbo = await connectToMongo();
+    await createCollection(dbo);
+    await insert(dbo);
+    console.log(await find(dbo));
+    console.log('xxx');
+}
+
+dosth();
